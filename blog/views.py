bb9ba5cc4e332f24455dd1b3fe99d40b454b0d8c
KLZzS2PQ <kicaevartem5@gmail.com>
2024-01-18 19:47:13 +0300

    fgdgdfg

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from django.http import HttpResponse
 
 
 def posts_list(request):
@@ -7,3 +8,12 @@ def posts_list(request):
         'posts': Post.objects.all()
     })
 
+# Страницу rename_post на которой будет 
+# форма для изменения имени поста.
+def post_like(request):
+    if request.method == 'POST':
+        post = Post.objects.get(id=request.POST['id'])  
+        post.likes += 1
+        post.save()
+    return redirect('posts_list')
+    
--- config/urls.py
+++ config/urls.py
@@ -3,22 +3,19 @@ from django.urls import path
 from Core.views import example, example2, magic_number
 from django.conf import settings
 from django.conf.urls.static import static
-from blog.views import posts_list
+from blog.views import posts_list, post_like 
 from playlist.views import playlist
 from playlist.views import create_video
-from pprint import pprint
-def sum():
-    return 0
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('examplderhserthwetrjwrtje_1/', example, name='example'),
+    path('exampld1/', example, name='example'),
     path('example2/', example2),
     path('magic_number/', magic_number),
-    path('posts_list/', posts_list, name='postlist'),
+    path('posts_list/', posts_list, name='posts_list'),
     path('playlist/', playlist, name='playlist'),
     path('create_video/', create_video),
+    path('post_like/', post_like, name='post_like')
 ]  
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    pprint(urlpatterns)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  
